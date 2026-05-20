1eceb5552097ee0c27d7f64891667fc470538d66
Haris-83 <haris@example.com>
2026-05-20 23:16:40 +0500

    fix: robust Firebase init with try-catch, fix SHA grep for Java 17+

--- lib/src/config/app_config.dart
+++ lib/src/config/app_config.dart
@@ -11,19 +11,21 @@ class AppConfig {
   static late final FirebaseStorage storage;
 
   static Future<void> init() async {
-    if (Firebase.apps.isEmpty) {
-      await Firebase.initializeApp(
-        options: FirebaseOptions(
-          apiKey: "AIzaSyB4HeD05C8qE5EJRqWmtGcBD46iAHQr8ZU",
-          authDomain: "chatapp-8a2d9.firebaseapp.com",
-          projectId: "chatapp-8a2d9",
-          storageBucket: "chatapp-8a2d9.firebasestorage.app",
-          messagingSenderId: "594203207025",
-          appId: "1:594203207025:web:498329a15816fe98bdc2d0",
-          measurementId: "G-5XBZE8Z4X6",
-        ),
-      );
-    }
+    try {
+      if (Firebase.apps.isEmpty) {
+        await Firebase.initializeApp(
+          options: FirebaseOptions(
+            apiKey: "AIzaSyB4HeD05C8qE5EJRqWmtGcBD46iAHQr8ZU",
+            authDomain: "chatapp-8a2d9.firebaseapp.com",
+            projectId: "chatapp-8a2d9",
+            storageBucket: "chatapp-8a2d9.firebasestorage.app",
+            messagingSenderId: "594203207025",
+            appId: "1:594203207025:web:498329a15816fe98bdc2d0",
+            measurementId: "G-5XBZE8Z4X6",
+          ),
+        );
+      }
+    } catch (_) {}
     auth = FirebaseAuth.instance;
     firestore = FirebaseFirestore.instance;
     storage = FirebaseStorage.instance;
