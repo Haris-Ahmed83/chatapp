8c0af2bbe6bca23c72dd4413eaef75e683a9aa2e
Haris-83 <haris@example.com>
2026-05-20 21:55:09 +0500

    fix: skip Firebase init if app already exists (Android duplicate fix)

--- lib/src/config/app_config.dart
+++ lib/src/config/app_config.dart
@@ -11,17 +11,19 @@ class AppConfig {
   static late final FirebaseStorage storage;
 
   static Future<void> init() async {
-    await Firebase.initializeApp(
-      options: FirebaseOptions(
-        apiKey: "AIzaSyB4HeD05C8qE5EJRqWmtGcBD46iAHQr8ZU",
-        authDomain: "chatapp-8a2d9.firebaseapp.com",
-        projectId: "chatapp-8a2d9",
-        storageBucket: "chatapp-8a2d9.firebasestorage.app",
-        messagingSenderId: "594203207025",
-        appId: "1:594203207025:web:498329a15816fe98bdc2d0",
-        measurementId: "G-5XBZE8Z4X6",
-      ),
-    );
+    if (Firebase.apps.isEmpty) {
+      await Firebase.initializeApp(
+        options: FirebaseOptions(
+          apiKey: "AIzaSyB4HeD05C8qE5EJRqWmtGcBD46iAHQr8ZU",
+          authDomain: "chatapp-8a2d9.firebaseapp.com",
+          projectId: "chatapp-8a2d9",
+          storageBucket: "chatapp-8a2d9.firebasestorage.app",
+          messagingSenderId: "594203207025",
+          appId: "1:594203207025:web:498329a15816fe98bdc2d0",
+          measurementId: "G-5XBZE8Z4X6",
+        ),
+      );
+    }
     auth = FirebaseAuth.instance;
     firestore = FirebaseFirestore.instance;
     storage = FirebaseStorage.instance;
